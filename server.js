b2dff4092cee12ec406d89a5eb1a3b0e0d3a8066
umeshn95 <umeshnamdev9@gmail.com>
2023-11-12 10:35:38 +0530

    localhost cors added

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const validateContactForm = [
 // Endpoint to handle contact form submissions with validation
 app.post('/api/contact', validateContactForm, async (req, res) => {
 
-    res.header('Access-Control-Allow-Origin', 'http://darwinanalytic.com');
+    res.header('Access-Control-Allow-Origin', 'http://http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
